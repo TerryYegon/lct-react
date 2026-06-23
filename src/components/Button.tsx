4731c56c7c2422068d7bd5081e544050d1a0eace
TerryYegon <yegonterry618@gmail.com>
2026-06-23 05:04:07 +0300

    advanced props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,27 @@
-import type { ReactNode } from "react"
+import type { ComponentProps, ReactNode } from "react"
 
 type ButtonProps = {
-    children :ReactNode
+variant?: "primary" | "secondary" | "ghost-destructive"
+} & ComponentProps<"button">
+
+
+export function Button({  variant ="primary", ...props }:ButtonProps){
+    return <button 
+    {...props}
+    
+    className ={getVariantStyle(variant) + " rounded px-2 py-1 transition-colors disabled:opacity-30 disabled:cursor-not-allowed"}></button>
 }
+function getVariantStyle(variant:variant){
+    switch(variant){
+        case "primary":
+            return "bg-violet-600 hover:bg-violet-500"
+        case "secondary":
+            return "bg-zinc-800 hover:bg-zinc-700"
+        case "ghost-destructive":
+            return " hover:bg-red text-red-500 hover:text-red-400"
 
 
-export function Button({ children  }:ButtonProps){
-    return <button className ="bg-violet-600 hover:bg-violet-500 rounded px-2 py-1 transition-colors disabled:opacity-30
-     disabled:cursor-not-allowed">{children}</button>
+        default:
+        throw new Error(`invalid variant: ${'variant satisfys never'}`)
+    }
 }
--- src/components/HabitList.tsx
+++ src/components/HabitList.tsx
@@ -1,7 +1,8 @@
+import { Button } from "./Button";
+import {endOfWeek, startOfWeek, format, eachDayOfInterval,} from "date-fns"
 export function HabitList(){
     const habits =[
         { id: 1, name: "Terry" },
-        { id: 2, name: "Yegon" }
     ];
     if (habits.length === 0) {
     return (<p className="text-zinc-500 py-12" >
@@ -10,9 +11,41 @@ export function HabitList(){
 }
 return <div className="flex flex-col gap-3">
 {habits.map(habit => (
-   <h1 key={habit.id}> {habit.name}</h1>
+   <HabitItem key={habit.id} habit ={habit}/>
 ))}
 
 </div>
 }
-fu
+
+type HabitItemProps ={
+    habit :{
+        id : "string"; name: "string"
+    }
+}
+function HabitItem( habit :HabitItemProps){
+
+    const visibleDate= eachDayOfInterval({
+        start: startOfWeek(new Date()),
+        end: endOfWeek(new Date())
+    })
+
+    return <div className ="rounded-xl bg-zinc-800 p-4 flex flex-col gap-3">
+       <div className="flex items-center justify-between">
+        
+        <div className="flex gap-3 items-center" >
+        <span className="font-medium">{habit. habit.name}</span>
+        <span className="text-sm text-amber-400">3 streak</span>
+        </div>
+        <Button variant="ghost-destructive">Delete</Button>
+
+        </div> 
+        <div className="flex gap-1.5">
+            {visibleDate.map(date => (
+                <Button key = {date.toISOString()} disabled ="true">
+          <span className="font-medium">{format(date, "EEE")}</span>
+        <span>2</span>
+                </Button>
+            ))}
+        </div>
+    </div>
+}
